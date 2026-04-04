93ffe5e1af96637a137c1a00ae61647ae9e4c2d9
Toroko <patthamon.chr@gmail.com>
2026-04-04 11:30:01 +0800

    updated

--- app.py
+++ app.py
@@ -28,3 +28,23 @@ predict_traffic = ai.predict_density(trip_hour, trip_day)
 recommended_pcm = logic.analyze_required_pcm(target_hours, ambient_temp, predict_traffic)
 
 #AI main dashboard display
+col1, col2, col3 = st.columns[3]
+col1.metric("Traffic density", f"{predict_traffic}")
+col2.metric("Estimated time", f"{target_hours* (1 + 0.5*predict_traffic):.1f} hrs")
+col3.metric("Recommended PCM", f"{recommended_pcm} g", delta = "+5% Safty")
+
+st.divider
+
+#Simulation segment
+st.header("Step 2: Real-Time Monitoring Simulation")
+if st.button("Start Delivery Simulation"):
+    progess_bar = st.progress(0)
+    status_text = st.empty()
+
+elapsed_time = st.slider("Simulate Elapsed Time (Hours)", 0.0, target_hours, 0.0)
+
+simulated_temp = 2.0 + (elapsed_time * 1.5) 
+current_trba = logic.calculate_rba(simulated_temp, ambient_temp, recommended_pcm, predict_traffic)
+
+st.metric("Predicted Internal Temp", f"{simulated_temp:.1f} °C")
+st.metric("Remaining Safety Window", f"{current_trba:.0f} mins")
