d4408ca2f7f38ee05dffb6015f6b37df050eb62d
Toroko <patthamon.chr@gmail.com>
2026-04-04 11:23:48 +0800

    monitoring simulation

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ st.title("Intelligent Bio-Logistics Ecosystem")
 def load_system():
     ai = traffic_prediction
     ai.train_from_csv('data\Traffic.csv')
+    logic = rba_calculator()
+    return ai, logic
+
+ai, logic = load_system()
+
+#Sidebar user input
+st.sidebar.header("Step 1: Pre-trip Planning")
+target_hours = st.sidebar.number_input("Standard travel duraton (hours)" , min_value=0.5, value=2.0)
+ambient_temp =st.sidebar.slider("Ambient Temoerature (°C)", 30, 50, 40)
+trip_hour = st.sidebar.slider("Departure hour", 0, 23, 8)
+trip_day = st.sidebar.selectbox("Day of week", options=[0,1,2,3,4,5,6], format_func=lambda x: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][x])
+
+#Logic AI analysis
+predict_traffic = ai.predict_density(trip_hour, trip_day)
+recommended_pcm = logic.analyze_required_pcm(target_hours, ambient_temp, predict_traffic)
+
+#AI main dashboard display
